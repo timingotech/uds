db0cdea1b283a8c08dbe89a85c97a97b68387bb8
Ridwan Oyenuga <taonuga01@gmail.com>
2024-06-01 12:55:18 +0100

    i made some changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import UnilagLogo from '../images/UnilagLogo.jfif';
+import UnilagLogo from '../images/UnilagLogo-removebg-preview.png';
 
 
 
